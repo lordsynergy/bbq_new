712528ac34a794c45227fa991ee76fe3d09c752c
lordsynergy <djordj88@mail.ru>
2023-01-02 13:31:46 +0300

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_scope :user do
     # Redirests signing out users back to sign-in
-    get "users", to: "devise/sessions#new"
+    get 'users', to: 'devise/sessions#new'
   end
 
   devise_for :users
-  root "events#index"
+  root 'events#index'
 
-  resources :events
+  resources :events do
+    resources :comments, only: %i[create destroy]
+  end
+
   resources :users, only: %i[show edit update]
 end
