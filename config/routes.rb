75488cba8f39851df511868fe7037bce2f121fdd
lordsynergy <djordj88@mail.ru>
2023-01-03 16:01:27 +0300

    subscriptions added

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :events do
     resources :comments, only: [:create, :destroy]
+    resources :subscriptions, only: [:create, :destroy]
   end
 
   resources :users, only: %i[show edit update]
