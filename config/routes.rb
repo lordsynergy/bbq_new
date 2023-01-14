613b96af594b1897cb4832171839b756dfa209bc
lordsynergy <djordj88@mail.ru>
2023-01-14 18:30:34 +0300

    add pincode  to event

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :comments, only: %i[index create destroy]
     resources :subscriptions, only: %i[index create destroy]
     resources :photos, only: %i[create destroy]
+
+    post :show, on: :member
   end
 
   resources :users, only: %i[show edit update]
