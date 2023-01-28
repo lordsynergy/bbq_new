8e1a613576cf6c8aa141ecccbfe154f3b59e61fa
lordsynergy <djordj88@mail.ru>
2023-01-28 12:27:30 +0300

    oauth fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,21 +7,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     oauth_service('Vkontakte')
   end
 
+  def yandex
+    oauth_service('Yandex')
+  end
+
   private
 
   def oauth_service(provider)
     if @user.persisted?
-      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider)
 
-      sign_in_and_redirect user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
 
-      redirect_to new_user_registration_url
+     redirect_to root_path
     end
   end
 
-  def set_user
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-  end
 end
