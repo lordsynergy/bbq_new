477281939e725127f7a8721f387cfef0a97ca314
lordsynergy <djordj88@mail.ru>
2023-01-28 13:28:38 +0300

    refactoring oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[github vkontakte]
+         :omniauthable, omniauth_providers: %i[github vkontakte yandex]
 
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
